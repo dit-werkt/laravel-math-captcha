570d1361ba86643318945d7d48960aff3367b388
Jan Alwin de Jong <janalwin@studio-stedum.nl>
2022-03-30 11:47:59 +0200

    Dutch translation

---
+++ src/resources/lang/nl/math-captcha.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'numbers' => [
+        '0' => 'Nul',
+        '1' => 'Een',
+        '2' => 'Twee',
+        '3' => 'Drie',
+        '4' => 'Vier',
+        '5' => 'Vijf',
+        '6' => 'Zes',
+        '7' => 'Zeven',
+        '8' => 'Acht',
+        '9' => 'Negen',
+        '10' => 'Tien',
+        '11' => 'Elf',
+        '12' => 'Twaalf',
+    ],
+    'operands' => [
+        '+' => 'plus',
+        '-' => 'min',
+        '*' => 'keer',
+        '/' => 'gedeeld door',
+    ],
+];
